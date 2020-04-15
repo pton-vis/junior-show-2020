327aa24bd67b200c015294c52ecf761ca3a6b687
Kevin Feng <kjfeng@princeton.edu>
2020-04-15 16:55:14 -0700

    toggle functional

---
+++ script.js
@@ -0,0 +1,17 @@
+function goToExpandedState() {
+  $('.window-neutral').hide();
+  $('.window-expanded').show();
+}
+
+function goToNeutralState() {
+  $('.window-neutral').show();
+  $('.window-expanded').hide();
+}
+
+$(document).ready(function() {
+  $('.p-button').click(function() {
+    $('.window-neutral').toggle();
+    $('.window-expanded').toggle();
+
+  });
+});
