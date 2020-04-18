d3411cada3f65ee271b210e856a4a37adb849730
Kevin Feng <kjfeng@princeton.edu>
2020-04-18 01:17:42 -0700

    comment out js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function goToExpandedState() {
-  $('.window-neutral').hide();
-  $('.window-expanded').show();
-}
-
-function goToNeutralState() {
-  $('.window-neutral').show();
-  $('.window-expanded').hide();
-}
+// function goToExpandedState() {
+//   $('.window-neutral').hide();
+//   $('.window-expanded').show();
+// }
+//
+// function goToNeutralState() {
+//   $('.window-neutral').show();
+//   $('.window-expanded').hide();
+// }
 
 $(document).ready(function() {
   $('.p-button').click(function() {
